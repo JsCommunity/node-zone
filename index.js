b70658cc6f230a64109c291d2fda5765842c4132
Julien Fontanet <julien.fontanet@isonoe.net>
2018-12-13 16:40:00 +0100

    chore: Husky/lint-staged/ESlint/Prettier

--- index.js
+++ index.js
@@ -1,108 +1,107 @@
-const zones = Object.create(null)
-let current
+const zones = Object.create(null);
+let current;
 
 // eslint-disable-next-line node/no-missing-require
-const asyncHooks = require('async_hooks')
+const asyncHooks = require("async_hooks");
 const asyncHook = asyncHooks.createHook({
-  init (uid) {
-    zones[uid] = current
+  init(uid) {
+    zones[uid] = current;
   },
 
-  before (uid) {
-    current = zones[uid]
+  before(uid) {
+    current = zones[uid];
   },
 
-  after (uid) {
-  },
+  after(uid) {},
 
-  destroy (uid) {
-    delete zones[uid]
+  destroy(uid) {
+    delete zones[uid];
   },
-})
-asyncHook.enable()
+});
+asyncHook.enable();
 
 class Zone {
-  constructor (parent, spec) {
+  constructor(parent, spec) {
     if (parent === null) {
       // root zone
-      this._data = Object.create(null)
-      this._name = '<root>'
-      this._parent = null
+      this._data = Object.create(null);
+      this._name = "<root>";
+      this._parent = null;
     } else {
-      const data = this._data = Object.create(parent.data)
-      this._parent = parent
+      const data = (this._data = Object.create(parent.data));
+      this._parent = parent;
 
-      let name
-      if (typeof spec === 'string') {
-        name = spec
+      let name;
+      if (typeof spec === "string") {
+        name = spec;
       } else if (spec != null) {
-        name = spec.name
+        name = spec.name;
 
-        const { properties } = spec
+        const { properties } = spec;
         if (properties) {
           for (const key in properties) {
-            data[key] = properties[key]
+            data[key] = properties[key];
           }
         }
       }
 
-      this._name = name || `${parent.name} child`
+      this._name = name || `${parent.name} child`;
     }
   }
 
-  get data () {
-    return this._data
+  get data() {
+    return this._data;
   }
 
-  get name () {
-    return this._name
+  get name() {
+    return this._name;
   }
 
-  get parent () {
-    return this._parent
+  get parent() {
+    return this._parent;
   }
 
-  fork (spec) {
-    return new Zone(this, spec)
+  fork(spec) {
+    return new Zone(this, spec);
   }
 
-  run (callback, thisArg, args) {
-    const previous = current
+  run(callback, thisArg, args) {
+    const previous = current;
     try {
-      current = this
-      return callback.apply(thisArg, args)
+      current = this;
+      return callback.apply(thisArg, args);
     } finally {
-      current = previous
+      current = previous;
     }
   }
 
-  wrap (callback) {
-    const zone = this
-    return function (...args) {
-      return zone.run(callback, this, args)
-    }
+  wrap(callback) {
+    const zone = this;
+    return function(...args) {
+      return zone.run(callback, this, args);
+    };
   }
 
   // minimal zone.js compatibility
 
-  get (key) {
-    return this._data[key]
+  get(key) {
+    return this._data[key];
   }
 
-  getZoneWith (key) {
+  getZoneWith(key) {
     if (Object.prototype.hasOwnProperty.call(this._data, key)) {
-      return this
+      return this;
     }
 
-    const parent = this._parent
-    return parent && parent.getZoneWith(key)
+    const parent = this._parent;
+    return parent && parent.getZoneWith(key);
   }
 }
 
-current = new Zone(null)
+current = new Zone(null);
 
 module.exports = {
-  get current () {
-    return current
+  get current() {
+    return current;
   },
-}
+};
--- index.spec.js
+++ index.spec.js
@@ -1,100 +1,100 @@
 /* eslint-env jest */
 
-const Zone = require('./')
+const Zone = require("./");
 
-const root = Zone.current
+const root = Zone.current;
 
-describe('Zone', () => {
-  describe('root zone', () => {
-    it('has “<root>” name', () => {
-      expect(root.name).toBe('<root>')
-    })
+describe("Zone", () => {
+  describe("root zone", () => {
+    it("has “<root>” name", () => {
+      expect(root.name).toBe("<root>");
+    });
 
-    it('has no parent', () => {
-      expect(root.parent).toBe(null)
-    })
+    it("has no parent", () => {
+      expect(root.parent).toBe(null);
+    });
 
-    it('has an object without prototype data', () => {
-      expect(root.data).toEqual({})
-      expect(Object.getPrototypeOf(root.data)).toBe(null)
-    })
-  })
+    it("has an object without prototype data", () => {
+      expect(root.data).toEqual({});
+      expect(Object.getPrototypeOf(root.data)).toBe(null);
+    });
+  });
 
-  describe('#fork()', () => {
-    it('creates a new child zone', () => {
-      const child = root.fork()
-      expect(child.name).toBe('<root> child')
-      expect(child.parent).toBe(root)
-      expect(child.data).toEqual({})
-      expect(Object.getPrototypeOf(child.data)).toBe(root.data)
-    })
+  describe("#fork()", () => {
+    it("creates a new child zone", () => {
+      const child = root.fork();
+      expect(child.name).toBe("<root> child");
+      expect(child.parent).toBe(root);
+      expect(child.data).toEqual({});
+      expect(Object.getPrototypeOf(child.data)).toBe(root.data);
+    });
 
-    it('accepts a name param', () => {
-      expect(root.fork('child').name).toBe('child')
-    })
+    it("accepts a name param", () => {
+      expect(root.fork("child").name).toBe("child");
+    });
 
-    it('accepts a ZoneSpec param', () => {
+    it("accepts a ZoneSpec param", () => {
       const child = root.fork({
-        name: 'child',
+        name: "child",
         properties: {
-          foo: 'bar',
+          foo: "bar",
         },
-      })
-      expect(child.name).toBe('child')
-      expect(child.data).toEqual({ foo: 'bar' })
-    })
-  })
+      });
+      expect(child.name).toBe("child");
+      expect(child.data).toEqual({ foo: "bar" });
+    });
+  });
 
-  describe('#run()', () => {
-    it('runs a callback in the zone', done => {
-      const child = root.fork()
+  describe("#run()", () => {
+    it("runs a callback in the zone", done => {
+      const child = root.fork();
 
       child.run(() => {
-        expect(Zone.current).toBe(child)
+        expect(Zone.current).toBe(child);
 
         process.nextTick(() => {
-          expect(Zone.current).toBe(child)
-          done()
-        })
-      })
-    })
-  })
+          expect(Zone.current).toBe(child);
+          done();
+        });
+      });
+    });
+  });
 
-  describe('#wrap()', () => {
-    it('creates a callback which runs in the zone', done => {
-      const child = root.fork()
+  describe("#wrap()", () => {
+    it("creates a callback which runs in the zone", done => {
+      const child = root.fork();
 
       child.wrap(() => {
-        expect(Zone.current).toBe(child)
+        expect(Zone.current).toBe(child);
 
         process.nextTick(() => {
-          expect(Zone.current).toBe(child)
-          done()
-        })
-      })()
-    })
-  })
+          expect(Zone.current).toBe(child);
+          done();
+        });
+      })();
+    });
+  });
 
-  describe('#get()', () => {
-    it('returns a value associated with a key', () => {
-      root.data.root = 'foo'
-      const child = root.fork()
-      child.data.child = 'bar'
+  describe("#get()", () => {
+    it("returns a value associated with a key", () => {
+      root.data.root = "foo";
+      const child = root.fork();
+      child.data.child = "bar";
 
-      expect(child.get('root')).toBe('foo')
-      expect(child.get('child')).toBe('bar')
-    })
-  })
+      expect(child.get("root")).toBe("foo");
+      expect(child.get("child")).toBe("bar");
+    });
+  });
 
-  describe('#getZoneWith()', () => {
-    it('returns the zone which defines a key', () => {
-      root.data.root = 'foo'
-      const child = root.fork()
-      child.data.child = 'bar'
+  describe("#getZoneWith()", () => {
+    it("returns the zone which defines a key", () => {
+      root.data.root = "foo";
+      const child = root.fork();
+      child.data.child = "bar";
 
-      expect(child.getZoneWith('root')).toBe(root)
-      expect(child.getZoneWith('child')).toBe(child)
-      expect(child.getZoneWith('foo')).toBe(null)
-    })
-  })
-})
+      expect(child.getZoneWith("root")).toBe(root);
+      expect(child.getZoneWith("child")).toBe(child);
+      expect(child.getZoneWith("foo")).toBe(null);
+    });
+  });
+});
